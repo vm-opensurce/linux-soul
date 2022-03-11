a67d713b65be7ee3d5d4deb5edfc2894b7a5bcbb
vm-opensurce <101404644+vm-opensurce@users.noreply.github.com>
2022-03-11 19:11:37 +0530

    Added core.c

---
+++ include/acpi/core.c
@@ -0,0 +1,30 @@
+/* 
+(C) Copyright 2022-2027 Venkatesh Mishra
+
+Soul linux is free and open sourced by Venkatesh Mishra.
+file: core.h
+compiler: gcc for x32 and clang for x86_64 and amd64, tcc for testing
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <unistdio.h>
+#include <curses.h>
+
+struct main {
+  char * bootloader;
+  int pid;
+};
+
+int short main(int argc, char * const argv[]){
+  int short i;
+  
+  
+  // coming soon
+  
+  return 0;
+}
